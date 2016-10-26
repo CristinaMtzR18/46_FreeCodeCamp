830d4fc8249fab98ec4ce715b306a2fcd1819008
CristinaMtzR18 <cris.mtz.r18@gmail.com>
2016-10-26 05:36:44 -0500

    Ejercicios y notas de FreeCodeCamp-Traduccion

---
+++ 1_Comentarios/js/main.js
@@ -0,0 +1,14 @@
+/*
+  Instrucciones.
+  Trate de crear un comentario de cada tipo.
+
+  -Crear un comentario estilo // que contiene al menos cinco letras.
+  -Crear un comentario / * * / estilo que contiene al menos cinco letras.
+  -Asegúrese de que se cierra el comentario con un * /.
+ */
+
+//Primer tipo de comentario. Una linea
+
+/*
+  Segundo tipo de comentario. Varias lineas
+ */
